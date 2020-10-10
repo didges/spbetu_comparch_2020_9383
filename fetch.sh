1c4c47c0a66b5fe7c56ae675f333d297e57053d8
JAkutenshi <jakutenshi@gmail.com>
2020-10-10 09:26:45 +0300

    Add fetch.sh scripts and student's githubs

---
+++ fetch.sh
@@ -0,0 +1,5 @@
+git fetch FollowJust     # Гладких
+git fetch Hotiakov       # Хотяков
+git fetch KamzolovNikita # Камзолов
+git fetch kirill-1103    # Крейсманн
+git fetch Kirill-Mois    # Моисейченко
---
+++ remotes.sh
@@ -0,0 +1,21 @@
+git remote add FollowJust     https://github.com/FollowJust/spbetu_comparch_2020_9383.git                  # Гладких
+git remote add Hotiakov       https://github.com/Hotiakov/spbetu_comparch_2020_9383.git                    # Хотяков
+git remote add KamzolovNikita https://github.com/KamzolovNikita/spbetu_comparch_2020_9383.git              # Камзолов
+git remote add kirill-1103    https://github.com/kirill-1103/spbetu_comparch_2020_9383.git                 # Крейсманн
+git remote add Kirill-Mois    https://github.com/Kirill-Mois/spbetu_comparch_2020_9383.git                # Моисейченко
+#git remote add                                  https://github.com//spbetu_os_2020_8381.git
+#git remote add                                  https://github.com//spbetu_os_2020_8381.git
+#git remote add                                  https://github.com//spbetu_os_2020_8381.git
+#git remote add                                  https://github.com//spbetu_os_2020_8381.git
+#git remote add                                  https://github.com//spbetu_os_2020_8381.git
+#git remote add                                  https://github.com//spbetu_os_2020_8381.git
+#git remote add                                  https://github.com//spbetu_os_2020_8381.git
+#git remote add                                  https://github.com//spbetu_os_2020_8381.git
+#git remote add                                  https://github.com//spbetu_os_2020_8381.git
+#git remote add                                  https://github.com//spbetu_os_2020_8381.git
+#git remote add                                  https://github.com//spbetu_os_2020_8381.git
+#git remote add                                  https://github.com//spbetu_os_2020_8381.git
+#git remote add                                  https://github.com//spbetu_os_2020_8381.git
+#git remote add                                  https://github.com//spbetu_os_2020_8381.git
+#git remote add                                  https://github.com//spbetu_os_2020_8381.git
+#git remote add                                  https://github.com//spbetu_os_2020_8381.git
