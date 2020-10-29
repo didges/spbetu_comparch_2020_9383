811e961756d09c25dab462009950278186a6b51f
didges <39410991+didges@users.noreply.github.com>
2020-10-29 10:59:03 +0300

    lab3 update

--- Poplavskiy/lab3/lab3.asm
+++ Poplavskiy/lab3/lab3.asm
@@ -3,10 +3,10 @@ AStack SEGMENT STACK
 AStack ENDS
 				
 DATA SEGMENT				
-	A 	DW 4
-	B 	DW 2
-	I 	DW 1
-	K 	DW 0
+	A 	DW 5
+	B 	DW 1
+	I 	DW 3
+	K 	DW -5
 	I1 	DW ?
 	I2 	DW ?
 	RES DW ?
@@ -19,40 +19,36 @@ Main PROC FAR
 	mov  ax, DATA
    	mov  ds, ax
 f1:
-	mov ax, A
-	mov bx, B
-	cmp ax, bx
+	cmp A, B
 	jg f1_1
-	jmp f1_2
-
-f1_1:
+	mov bx, I
 	mov ax, I
+	shl bx, 1
+	shl bx, 1
+	shl bx, 1
 	shl ax, 1
-	shl ax, 1
-	mov bx, 7
 	sub bx, ax
-	mov I1, bx
+	mov ax, 8
+	sub ax, bx
+	mov I1, ax
 	jmp f2
 
-f1_2:
-	mov ax, I
+f1_1:
 	mov bx, I
-	shl ax, 1
-	shl ax, 1
-	shl ax, 1
 	shl bx, 1
+	shl bx, 1
+	mov ax, 7
 	sub ax, bx
-	mov bx, 8
-	sub bx, ax
-	mov I1, bx
+	mov I1, ax
 	jmp f2
 
 f2:
-	mov ax, A
-	mov bx, B
-	cmp ax, bx
+	cmp A, B
 	jg f2_1
-	jmp f2_2
+	shr ax, 1
+	sub ax, 2	
+	mov I2, ax
+	jmp f3
 
 f2_1:
 	mov ax, I
@@ -62,49 +58,47 @@ f2_1:
 	mov I2, ax
 	jmp f3
 
-f2_2:
-	mov ax, I
-	mov bx, I
-	shl bx, 1
-	shl bx, 1
-	sub ax, bx
-	mov bx, 2
-	add ax, bx
-	mov I2, ax
-	jmp f3
-
 f3:
-	mov bx, K
-	cmp bx, 0
-	je f3_1
-	jmp f3_2
-
-
-f3_1:
+	cmp K, 0
+	jg f3_1
 	mov ax, I1
 	mov bx, I2
-	add ax, bx
-	cmp ax, 0
-	jg f3_neg
+	sub ax, bx
+	mov bx, 0
+	cmp bx, ax
+	jg f3_2_neg
+	jmp f3_2_1
+
+f3_1:
+	mov ax, I2
+	neg ax
+	mov bx, 6
+	neg bx
+	cmp ax, bx
+	jg f3_1_1
+	mov RES, bx
+	jmp f_end 
+
+f3_1_1:
 	mov RES, ax
 	jmp f_end
 
-f3_neg:
-	neg ax
-	jmp f3_1
 
-f3_2:
-	mov ax, I1
-	mov bx, I2
+f3_2_neg:
+	neg ax
+	jmp f3_2_1
+
+f3_2_1:
+	mov bx, 2
 	cmp ax, bx
-	jb f_3_2_1
+	jg f3_2_1_a
+	mov RES, ax
+	jmp f_end
+
+f3_2_1_a:
 	mov RES, bx
 	jmp f_end
 
-f_3_2_1:
-	mov RES, ax
-	jmp f_end
-		
 f_end:
 	mov  ah, 4ch		
 	int  21h  
@@ -114,9 +108,3 @@ f_end:
 Main      ENDP
 CODE      ENDS
 END Main
-	
-
-	
-	
-	
-		
